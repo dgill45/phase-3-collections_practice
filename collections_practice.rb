4030ad7691671e1b00cf5dcb82a9cdf5a5ba4b9a
donellgill <donell.gill@outlook.com>
2021-07-04 20:18:04 -0400

    done

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
 
+numbers = [4, 26, 32, 21, 16]
+
+def sort_array_asc(numbers)
+    numbers.sort
+end
+
+def sort_array_desc(numbers)
+    numbers.sort.reverse
+end
+
+def sort_array_char_count(string)
+    string.sort do |a, b|
+        a.length <=> b.length
+    end
+end
